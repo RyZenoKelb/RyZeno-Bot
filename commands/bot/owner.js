d3f020eac0efe9596bb9214c83bd9c77807b10aa
RyZeno <ryzenoking@gmail.com>
2024-05-16 19:04:30 +0200

    2

--- commands/bot/owner.js
+++ commands/bot/owner.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require('discord.js');
+const db = require('quick.db');
 const {
     MessageActionRow,
     MessageButton,
@@ -13,37 +13,26 @@ module.exports = {
     run: async (client, message, args, prefix, color) => {
 
         if (client.config.owner.includes(message.author.id)) {
-
-            if (args[0] === undefined) {
-                let member = client.users.cache.get(message.author.id);
-                if (args[0]) {
-                    member = client.users.cache.get(args[0]);
-                } else {
-                    return message.channel.send(`Aucun membre trouvé pour \`${args[0] || " "}\``)
+            if (args.length === 1) { // Si la longueur de args est de 1, cela signifie qu'un utilisateur a été fourni
+                let member = client.users.cache.get(args[0]) || message.mentions.users.first();
+                if (!member) {
+                    return message.channel.send(`Aucun membre trouvé pour \`${args[0] || " "}\``);
                 }
-                if (message.mentions.members.first()) {
-                    member = client.users.cache.get(message.mentions.members.first().id);
-                }
-                if (!member) return message.channel.send(`Aucun membre trouvé pour \`${args[0] || " "}\``)
                 if (db.get(`ownermd_${client.user.id}_${member.id}`) === true) {
-                    return message.channel.send(`<@${member.id}> est déjà owner`)
+                    return message.channel.send(`<@${member.id}> est déjà owner`);
                 }
 
-                db.set(`ownermd_${client.user.id}_${member.id}`, true)
-
-                message.channel.send(`<@${member.id}> est maintenant owner`)
+                db.set(`ownermd_${client.user.id}_${member.id}`, true);
+                message.channel.send(`<@${member.id}> est maintenant owner`);
             } else if (args[0] === "clear") {
                 let tt = await db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`));
-                message.channel.send(`${tt.length === undefined || null ? 0 : tt.length} ${tt.length > 1 ? "personnes ont été supprimées " : "personne a été supprimée"} des owners`)
+                message.channel.send(`${tt.length === undefined || null ? 0 : tt.length} ${tt.length > 1 ? "personnes ont été supprimées " : "personne a été supprimée"} des owners`);
 
-
-                let delowner = 0;
                 for (let i = 0; i < tt.length; i++) {
                     db.delete(tt[i].ID);
-                    delowner++;
                 }
             } else if (args[0] === "list") {
-                let money = db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`)).sort((a, b) => b.data - a.data)
+                let money = db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`)).sort((a, b) => b.data - a.data);
 
                 let p0 = 0;
                 let p1 = 5;
@@ -55,11 +44,9 @@ module.exports = {
                         .filter(x => client.users.cache.get(x.ID.split('_')[2]))
                         .map((m, i) => `${i + 1}) <@${client.users.cache.get(m.ID.split('_')[2]).id}> (${client.users.cache.get(m.ID.split('_')[2]).id})`)
                         .slice(0, 5)
-
                     )
                     .setFooter(`${page}/${Math.ceil(money.length === 0 ? 1 : money.length / 5)} • ${client.config.name}`)
-                    .setColor(color)
-
+                    .setColor(color);
 
                 message.channel.send(embed).then(async tdata => {
                     if (money.length > 5) {
@@ -75,11 +62,13 @@ module.exports = {
 
                         const bts = new MessageActionRow()
                             .addComponent(B1)
-                            .addComponent(B2)
+                            .addComponent(B2);
+
                         tdata.edit("", {
                             embed: embed,
                             components: [bts]
-                        })
+                        });
+
                         setTimeout(() => {
                             tdata.edit("", {
                                 components: [],
@@ -89,76 +78,52 @@ module.exports = {
                                         .filter(x => client.users.cache.get(x.ID.split('_')[2]))
                                         .map((m, i) => `${i + 1}) <@${client.users.cache.get(m.ID.split('_')[2]).id}> (${client.users.cache.get(m.ID.split('_')[2]).id})`)
                                         .slice(0, 5)
-
                                     )
                                     .setFooter(`1/${Math.ceil(money.length === 0 ? 1 : money.length / 5)} • ${client.config.name}`)
                                     .setColor(color)
+                            });
+                        }, 60000 * 5);
 
-
-                            })
-                            // message.channel.send(embeds)
-                        }, 60000 * 5)
                         client.on("clickButton", (button) => {
                             if (button.clicker.user.id !== message.author.id) return;
                             if (button.id === "owner1") {
-                                button.reply.defer(true)
+                                button.reply.defer(true);
 
                                 p0 = p0 - 5;
                                 p1 = p1 - 5;
-                                page = page - 1
+                                page = page - 1;
 
-                                if (p0 < 0) {
-                                    return
-                                }
-                                if (p0 === undefined || p1 === undefined) {
-                                    return
-                                }
-
+                                if (p0 < 0) return;
 
                                 embed.setDescription(money
                                     .filter(x => client.users.cache.get(x.ID.split('_')[2]))
                                     .map((m, i) => `${i + 1}) <@${client.users.cache.get(m.ID.split('_')[2]).id}> (${client.users.cache.get(m.ID.split('_')[2]).id})`)
                                     .slice(p0, p1)
-
                                 )
-                                    .setFooter(`${page}/${Math.ceil(money.length === 0 ? 1 : money.length / 5)} • ${client.config.name}`)
+                                .setFooter(`${page}/${Math.ceil(money.length === 0 ? 1 : money.length / 5)} • ${client.config.name}`);
                                 tdata.edit(embed);
-
                             }
                             if (button.id === "owner2") {
-                                button.reply.defer(true)
+                                button.reply.defer(true);
 
                                 p0 = p0 + 5;
                                 p1 = p1 + 5;
-
                                 page++;
 
-                                if (p1 > money.length + 5) {
-                                    return
-                                }
-                                if (p0 === undefined || p1 === undefined) {
-                                    return
-                                }
-
+                                if (p1 > money.length + 5) return;
 
                                 embed.setDescription(money
                                     .filter(x => client.users.cache.get(x.ID.split('_')[2]))
                                     .map((m, i) => `${i + 1}) <@${client.users.cache.get(m.ID.split('_')[2]).id}> (${client.users.cache.get(m.ID.split('_')[2]).id})`)
                                     .slice(p0, p1)
-
                                 )
-                                    .setFooter(`${page}/${Math.ceil(money.length === 0 ? 1 : money.length / 5)} • ${client.config.name}`)
+                                .setFooter(`${page}/${Math.ceil(money.length === 0 ? 1 : money.length / 5)} • ${client.config.name}`);
                                 tdata.edit(embed);
-
                             }
-                        })
+                        });
                     }
-
-                })
-
+                });
             }
         }
-
-
     }
 }
